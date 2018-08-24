4c824481bb42508ce7bc42e3fc6ba50fdf6337df
DoubleThink1984 <d.a.cook1@outlook.com>
2018-08-24 14:58:02 -0500

    add stuff

--- WebApi2Book.Web.Api.Models/Task.cs
+++ WebApi2Book.Web.Api.Models/Task.cs
@@ -1,33 +1,61 @@
-﻿using System;
+﻿// Task.cs
+// Copyright Jamie Kurtz, Brian Wortman 2014.
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi2Book.Web.Api.Models
 {
-    public class Task
+    public class Task : ILinkContaining
     {
         private List<Link> _links;
+        private bool _shouldSerializeAssignees;
 
-        public virtual long? TaskId { get; set; }
-        public virtual string Subject { get; set; }
-        public virtual DateTime? StartDate { get; set; }
-        public virtual DateTime? DueDate { get; set; }
-        public virtual DateTime? CompletedDate { get; set; }
-        public virtual Status Status { get; set; }
-        public virtual DateTime? CreatedDate { get; set; }
-        public virtual List<User> Assignees { get; set; }
+        [Key]
+        public long? TaskId { get; set; }
 
+        [Editable(true)]
+        public string Subject { get; set; }
+
+        [Editable(true)]
+        public DateTime? StartDate { get; set; }
+
+        [Editable(true)]
+        public DateTime? DueDate { get; set; }
+
+        [Editable(false)]
+        public DateTime? CreatedDate { get; set; }
+
+        [Editable(false)]
+        public DateTime? CompletedDate { get; set; }
+
+        [Editable(false)]
+        public Status Status { get; set; }
+
+        [Editable(false)]
+        public List<User> Assignees { get; set; }
+
+        [Editable(false)]
         public List<Link> Links
         {
             get { return _links ?? (_links = new List<Link>()); }
-            set { _links = value;  }
+            set { _links = value; }
         }
 
         public void AddLink(Link link)
         {
             Links.Add(link);
+        }
+
+        public void SetShouldSerializeAssignees(bool shouldSerialize)
+        {
+            _shouldSerializeAssignees = shouldSerialize;
+        }
+
+        public bool ShouldSerializeAssignees()
+        {
+            return _shouldSerializeAssignees;
         }
     }
 }
